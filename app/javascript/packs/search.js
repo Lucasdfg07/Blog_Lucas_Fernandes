ef19c458df52dff40f60472449b4ab97d7dd2eb9
Lucasdfg07 <lucassiqueirafernandes07@gmail.com>
2021-05-28 18:30:56 -0300

    Setting pointer on blog

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -2,10 +2,21 @@ import $ from 'jquery';
 import "select2";
 import 'select2/dist/css/select2.css';
 
-$(() =>
-  $( "#dropdown" ).select2()
-);
+
+$(document).on('turbolinks:load', function() {
+  let el = $('#dropdown');
+  if ($(el).data('select2') == undefined && $(el).next().hasClass('select2-container')) {
+    $(el).remove();
+  }
+  $(el).select2();
+});
 
 $(document).ready(function() {
-  $( ".dash" ).hide(1000)
+    let placeholder_digit = $('.select2-selection__rendered')
+    placeholder_digit.html(placeholder_digit[0].outerText + '<span class="pointer">|</span>')
+    window.setInterval(setPointer, 800);
 })
+
+function setPointer() {
+  $('.pointer').toggle();
+}
