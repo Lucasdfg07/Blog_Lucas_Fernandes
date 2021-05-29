71e5131c3e11dc913c74371b92025b6e7b8a5b77
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2021-05-29 19:01:53 -0300

    Setting audio config

--- app/javascript/packs/audio.js
+++ app/javascript/packs/audio.js
@@ -1,5 +1,10 @@
 import $ from 'jquery';
 
+$(document).ready(function() {
+    let audio = new Audio('/intro.mp3')
+    audio.play()
+})
+
 $(() =>
     $(document).on('turbolinks:click', function() {
         $('audio').remove();
